c553571c13c38d26c6920d97cb8d575e21aa02e2
nick <nick@fiete.me>
2022-01-18 16:22:22 -0600

    UI and vi improvements

--- gtk_frontend.py
+++ gtk_frontend.py
@@ -7,9 +7,35 @@ gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk, Pango
 from gi.repository import Gdk
 
+
+
+class SearchDialog(Gtk.Dialog):
+    def __init__(self, parent):
+        super().__init__(title="Search", transient_for=parent, modal=True)
+        self.add_buttons(
+            "Exact",
+            0,
+            "Regex",
+            1,
+            Gtk.STOCK_CANCEL,
+            -1,
+        )
+
+        box = self.get_content_area()
+
+        label = Gtk.Label(label="Search")
+        box.add(label)
+
+        self.entry = Gtk.Entry()
+        box.add(self.entry)
+
+        self.show_all()
+
+
 class TextViewWindow(Gtk.Window):
-    def __init__(self, text):
-        Gtk.Window.__init__(self, title="tjna")
+    def __init__(self, name, text):
+        Gtk.Window.__init__(self, title=name)
+        self.title = name
 
         self.set_default_size(-1, 350)
 
@@ -26,10 +52,25 @@ class TextViewWindow(Gtk.Window):
         #self.textview.set_left_margin(100)
         self.create_buttons()
         self.connect("key-press-event",self.on_key_press_event)
+        self.to_begining()
+
+    def to_begining(self):
+        self.textbuffer.place_cursor(self.textview.get_buffer().get_iter_at_offset(0))
+
+    def set_text(self, new_text):
+        self.textbuffer.set_text(new_text)
 
     def on_key_press_event(self, widget, event):
         key_name = Gdk.keyval_name(event.keyval)
-        if key_name == 'l':
+        if key_name == 'Escape':
+            print(not self.textview.get_editable())
+            self.textview.set_editable(not self.textview.get_editable())
+        elif key_name == 't':
+            self.set_text('blah blah blah')
+            #dialog = SearchDialog(self)
+            #response = dialog.run()
+            #dialog.destroy()
+        elif key_name == 'l':
             buf = self.textview.get_buffer()
             cur_cur = buf.get_iter_at_mark(buf.get_insert())
             cur_cur.forward_chars(1)
@@ -40,17 +81,22 @@ class TextViewWindow(Gtk.Window):
             cur_cur.backward_chars(1)
             buf.place_cursor(cur_cur)
         elif key_name == 'j':
-            buf = self.textview.get_buffer()
-            cur_cur = buf.get_iter_at_mark(buf.get_insert())
-            self.textview.forward_display_line(cur_cur)
-            buf.place_cursor(cur_cur)
+            # TODO: There is a better way to do this probably. In the gtk source
+            # code they use gtk_widget_class_add_binding_signal (gtktextview.c
+            # line 800 is where the use it defined on 4328 of gtkwidget.c) But
+            # this aappears to either not be in the python version or I don't
+            # know how to do it. If it is the former I might see if I can add it
+            # in later of the ladder I will switch.
+            cur_cur = self.textbuffer.get_iter_at_mark(self.textbuffer.get_insert())
+            loc = self.textview.get_cursor_locations()[0]
+            new_cur = self.textview.get_iter_at_location(loc.x, loc.y + loc.height*1.1)[1]
+            self.textbuffer.place_cursor(new_cur)
         elif key_name == 'k':
-            buf = self.textview.get_buffer()
-            cur_cur = buf.get_iter_at_mark(buf.get_insert())
-            offset = cur_cur.get_visible_line_offset()
-            cur_cur.backward_lines(1)
-            cur_cur.set_visible_line_offset(offset)
-            buf.place_cursor(cur_cur)
+            # TODO: see above. Also this doesn't totally work for paragraphs
+            cur_cur = self.textbuffer.get_iter_at_mark(self.textbuffer.get_insert())
+            loc = self.textview.get_cursor_locations()[0]
+            new_cur = self.textview.get_iter_at_location(loc.x, loc.y - loc.height*.1)[1]
+            self.textbuffer.place_cursor(new_cur)
         elif key_name == 'q':
             buf = self.textview.get_buffer()
             cur_cur = buf.get_iter_at_mark(buf.get_insert())
@@ -63,7 +109,7 @@ class TextViewWindow(Gtk.Window):
             new = ''
             for word in words:
                 new += word + "\n"
-            win = TextViewWindow(new)
+            win = TextViewWindow(self.title + '_0', new)
             win.show_all()
         elif key_name == 'w':
             buf = self.textbuffer
@@ -79,10 +125,12 @@ class TextViewWindow(Gtk.Window):
             new = ''
             for word in results:
                 new += word + "---------------------------------\n"
-            win = TextViewWindow(new)
+            win = TextViewWindow(self.title + '_0', new)
             win.show_all()
 
-        elif key_name == 'a':
+            print(key_name)
+
+        elif key_name == 'g':
             self.textview.get_buffer().place_cursor(self.textview.get_buffer().get_iter_at_offset(0))
         elif key_name == 'p':
             self.size +=1
@@ -107,7 +155,7 @@ class TextViewWindow(Gtk.Window):
 
         self.textview = Gtk.TextView()
         self.textbuffer = self.textview.get_buffer()
-        self.textbuffer.set_text( my_text)
+        self.textbuffer.set_text(my_text)
         scrolledwindow.add(self.textview)
 
         self.tag_bold = self.textbuffer.create_tag("bold", weight=Pango.Weight.BOLD)
@@ -175,8 +223,4 @@ class TextViewWindow(Gtk.Window):
         self.textview.set_justification(justification)
 
 
-my_text = open('wagahai_wa_neko_de_aru', 'r').read()
-win = TextViewWindow(my_text)
-win.connect("destroy", Gtk.main_quit)
-win.show_all()
-Gtk.main()
+
