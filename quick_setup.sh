2277ff88c6d52fdc0b50ee0ce6744fb3f9c876b6
nick <nick@fiete.me>
2022-04-04 17:15:50 -0500

    fix links

--- quick_setup.sh
+++ quick_setup.sh
@@ -2,14 +2,22 @@
 
 set -e
 
-jm_url='https://foosoft.net/projects/yomichan/dl/dict/jmdict_english.zip'
-kanji_url='https://foosoft.net/projects/yomichan/dl/dict/kanjidic_english.zip'
-mkdir ./converters/dicts
-mkdir ./converters/kanji_dicts
+jm_url='https://github.com/FooSoft/yomichan/raw/master/dl/dict/jmdict_english.zip'
+kanji_url='https://github.com/FooSoft/yomichan/raw/master/dl/dict/kanjidic_english.zip'
+if [ ! -d ./converters/dicts ]; then
+	mkdir ./converters/dicts
+fi
+if [ ! -d ./converters/kanji_dicts ];then
+	mkdir ./converters/kanji_dicts
+fi
 wget "$jm_url" -O ./jm.zip
 wget "$kanji_url" -O ./kanji.zip
-mkdir ./converters/dicts/jm
-mkdir ./converters/kanji_dicts/kanji
+if [ ! -d ./converters/dicts/jm ];then
+	mkdir ./converters/dicts/jm
+fi
+if [ ! -d ./converters/dicts/kanji_dicts/kanji];then
+	 mkdir ./converters/kanji_dicts/kanji
+fi
 unzip jm.zip -d ./converters/dicts/jm
 unzip kanji.zip -d ./converters/kanji_dicts/kanji
 python3 ./converters/add_yomichan_dict.py ./converters/dicts
@@ -18,5 +26,7 @@ python3 ./converters/add_yomi_conj_dict.py ./converters/deinflect.json
 sudo ln -s "$(pwd)/main.py" /usr/bin/dire
 sudo ln -s "$(pwd)/dire_send_text.py" /usr/bin/dire_send_text
 sudo ln -s "$(pwd)/dire_cli" /usr/bin/dire_cli
-sudo mkdir /usr/share/dire
+if [ ! -d /usr/share/dire ];then
+	sudo mkdir /usr/share/dire
+fi
 sudo cp default_config.py /usr/share/dire/config.py
